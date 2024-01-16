b8f6917ddd0705478fbd253c69f453767f8cc83f
Lalit Kumawat <lalitkumawat1129@gmail.com>
2024-01-16 16:30:05 +0530

    Sidebar is working fine

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,12 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-
 import "./index.css";
 import { ApolloProvider } from "@apollo/client";
 import { client } from "./apolloClient.ts";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import Home from "./pages/Home.tsx";
-import { MantineProvider } from "@mantine/core";
 
 const router = createBrowserRouter([
   {
@@ -19,10 +17,8 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <MantineProvider >
       <ApolloProvider client={client}>
         <RouterProvider router={router} />
       </ApolloProvider>
-    </MantineProvider>
   </React.StrictMode>
 );
