d6dd2c80a8d5283ba4875b502f54322594b04a9f
Lalit Kumawat <lalitkumawat1129@gmail.com>
2024-01-18 17:07:58 +0530

    experimenting to solve unautherize error

--- frontend/src/apolloClient.ts
+++ frontend/src/apolloClient.ts
@@ -1,18 +1,19 @@
 import {
   ApolloClient,
   InMemoryCache,
-  NormalizedCacheObject,
-  gql,
-  Observable,
-  ApolloLink,
+  // NormalizedCacheObject,
+  // gql,
+  // Observable,
+  // ApolloLink,
   split,
+  HttpLink,
 } from "@apollo/client"
 // import { WebSocketLink } from "@apollo/client/link/ws"
-import { createUploadLink } from "apollo-upload-client"
+// import { createUploadLink } from "apollo-upload-client"
 import { getMainDefinition } from "@apollo/client/utilities"
 import { loadErrorMessages, loadDevMessages } from "@apollo/client/dev"
-import { useUserStore } from "./stores/userStore"
-import { onError } from "@apollo/client/link/error"
+// import { useUserStore } from "./stores/userStore"
+// import { onError } from "@apollo/client/link/error"
 import { GraphQLWsLink } from '@apollo/client/link/subscriptions';
 import { createClient } from 'graphql-ws';
 
@@ -21,26 +22,53 @@ import { createClient } from 'graphql-ws';
 loadErrorMessages()
 loadDevMessages()
 
-async function refreshToken(client: ApolloClient<NormalizedCacheObject>) {
-  try {
-    const { data } = await client.mutate({
-      mutation: gql`
-        mutation RefreshToken {
-          refreshToken
-        }
-      `,
-    })
-    const newAccessToken = data?.refreshToken
-    if (!newAccessToken) {
-      throw new Error("New access token not received.")
-    }
-    return `Bearer ${newAccessToken}`
-  } catch (err) {
-    throw new Error("Error getting new access token.")
-  }
-}
-let retryCount = 0
-const maxRetry = 3
+// async function refreshToken(client: ApolloClient<NormalizedCacheObject>) {
+//   try {
+//     const { data } = await client.mutate({
+//       mutation: gql`
+//         mutation RefreshToken {
+//           refreshToken
+//         }
+//       `,
+//     })
+//     const newAccessToken = data?.refreshToken
+//     if (!newAccessToken) {
+//       throw new Error("New access token not received.")
+//     }
+//     return `Bearer ${newAccessToken}`
+//   } catch (err) {
+//     throw new Error("Error getting new access token.")
+//   }
+// }
+
+const httpLink = new HttpLink({
+  uri: 'http://localhost:3000/graphql'
+});
+
+const wsLink = new GraphQLWsLink(createClient({
+  url: 'wss://real-time-chat-vewr.onrender.com/graphql',
+  shouldRetry: () => true,
+  connectionParams: {
+    Authorization: `Bearer ${localStorage.getItem("accessToken")}`,
+  },
+}));
+
+
+const splitLink = split(
+  ({ query }) => {
+    const definition = getMainDefinition(query);
+    return (
+      definition.kind === 'OperationDefinition' &&
+      definition.operation === 'subscription'
+    );
+  },
+  wsLink,
+  httpLink,
+);
+
+// let retryCount = 0
+// const maxRetry = 3
+
 
 // const wsLink = new WebSocketLink({
 //   uri: `wss://real-time-chat-vewr.onrender.com/graphql`,
@@ -52,68 +80,63 @@ const maxRetry = 3
 //   },
 // })
 
-const wsLink = new GraphQLWsLink(createClient({
-  url: 'wss://real-time-chat-vewr.onrender.com/graphql',
-  shouldRetry: () => true,
-  connectionParams: {
-    Authorization: `Bearer ${localStorage.getItem("accessToken")}`,
-  },
-}));
+// const errorLink = onError(({ graphQLErrors, operation, forward }) => {
+//   if(graphQLErrors?.length){
+//   for (const err of graphQLErrors) {
+//     if (err.extensions.code === "UNAUTHENTICATED" && retryCount < maxRetry) {
+//       retryCount++
+//       return new Observable((observer) => {
+//         refreshToken(client)
+//           .then((token) => {
+//             console.log("token", token)
+//             operation.setContext((previousContext: any) => ({
+//               headers: {
+//                 ...previousContext.headers,
+//                 authorization: token,
+//               },
+//             }))
+//             const forward$ = forward(operation)
+//             forward$.subscribe(observer)
+//           })
+//           .catch((error) => observer.error(error))
+//       })
+//     }
+
+//     if (err.message === "Refresh token not found") {
+//       console.log("refresh token not found!")
+//       useUserStore.setState({
+//         id: undefined,
+//         fullname: "",
+//         email: "",
+//       })
+//     }
+//   }
+// }
+// })
+
+// const uploadLink = createUploadLink({
+//   uri: "https://real-time-chat-vewr.onrender.com/graphql",
+//   credentials: "include",
+//   headers: {
+//     "apollo-require-preflight": "true",
+//   },
+// })
 
 
-const errorLink = onError(({ graphQLErrors, operation, forward }) => {
-  if(graphQLErrors?.length){
-  for (const err of graphQLErrors) {
-    if (err.extensions.code === "UNAUTHENTICATED" && retryCount < maxRetry) {
-      retryCount++
-      return new Observable((observer) => {
-        refreshToken(client)
-          .then((token) => {
-            console.log("token", token)
-            operation.setContext((previousContext: any) => ({
-              headers: {
-                ...previousContext.headers,
-                authorization: token,
-              },
-            }))
-            const forward$ = forward(operation)
-            forward$.subscribe(observer)
-          })
-          .catch((error) => observer.error(error))
-      })
-    }
+// const splitLink = split(
+//   // Split based on operation type
+//   ({ query }) => {
+//     const definition = getMainDefinition(query)
+//     return (
+//       definition.kind === "OperationDefinition" &&
+//       definition.operation === "subscription"
+//     )
+//   },
+//   wsLink,
+//   ApolloLink.from([errorLink, uploadLink])
+// )
 
-    if (err.message === "Refresh token not found") {
-      console.log("refresh token not found!")
-      useUserStore.setState({
-        id: undefined,
-        fullname: "",
-        email: "",
-      })
-    }
-  }
-}
-})
 
-const uploadLink = createUploadLink({
-  uri: "https://real-time-chat-vewr.onrender.com/graphql",
-  credentials: "include",
-  headers: {
-    "apollo-require-preflight": "true",
-  },
-})
-const link = split(
-  // Split based on operation type
-  ({ query }) => {
-    const definition = getMainDefinition(query)
-    return (
-      definition.kind === "OperationDefinition" &&
-      definition.operation === "subscription"
-    )
-  },
-  wsLink,
-  ApolloLink.from([errorLink, uploadLink])
-)
 export const client = new ApolloClient({
   uri: "https://real-time-chat-vewr.onrender.com/graphql",
   cache: new InMemoryCache({}),
@@ -122,5 +145,5 @@ export const client = new ApolloClient({
     "Content-Type": "application/json",
   
   },
-  link: link,
+  link: splitLink,
 })
